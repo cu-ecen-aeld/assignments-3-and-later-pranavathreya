1f5de6e514f514991a7550e7697f1b72c3996664
Pranav Athreya <pranavsubbu@gmail.com>
2024-03-13 23:15:52 +0530

    debug

--- full-test.sh
+++ full-test.sh
@@ -17,7 +17,7 @@ logfile=test.sh.log
 exec > >(tee -i -a "$logfile") 2> >(tee -i -a "$logfile" >&2)
 
 echo "Running test with user $(whoami)"
-echo "ls /home: "$(ls /home)
+echo "find . -name aarch64-linux-gnu: "$(find . -name \"*aarch64-linux-gnu*\")
 exit
 
 set +e
